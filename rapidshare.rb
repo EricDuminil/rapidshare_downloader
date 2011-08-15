f33b331da31117fd800b731a9d78f9909bfa3905
Eric Duminil <eric.duminil@gmail.com>
2011-08-15 12:51:00 +0200

    grep was the wrong method

--- rapidshare.rb
+++ rapidshare.rb
@@ -4,7 +4,7 @@ require 'download_helper'
 require 'load_configuration'
 
 
-download_list=File.read(InputFile).gsub(/https?:\/\/rapidshare.com\/#!download\|.*?\|(\d+)\|(.*?)\|\d+\|.*/,'https://rapidshare.com/files/\1/\2').grep(RapidShareURL){[$1,$2,$3]}.uniq
+download_list=File.read(InputFile).gsub(/https?:\/\/rapidshare.com\/#!download\|.*?\|(\d+)\|(.*?)\|\d+\|.*/,'https://rapidshare.com/files/\1/\2').scan(RapidShareURL)
 
 downloads_number=download_list.size
 
